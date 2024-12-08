0bcf9900ca005edf1d3d138164aa98b75b482459
aidansunbury <aidansunbury@gmail.com>
2024-12-08 15:22:43 -0800

    test workflow by adding unused import

--- src/components/JoinGroup.tsx
+++ src/components/JoinGroup.tsx
@@ -7,7 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
